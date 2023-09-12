d43316be5ef747854d6cbb87dddddd5b92ccc49b
rzelafaj <1736462009@mail.utec.edu.sv>
2023-09-12 09:46:56 -0600

    CAMBIOS EN LA CARPETA DE CONTROLES

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace INNOVAMED.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        //GET: Home
         public ActionResult Index()
         {
             return View();
