298c6f596fb781ee3a0c75e79f2f90a38d0d1139
Stanley Ramirez <stanleyhernandez74@gmail.com>
2023-09-14 08:22:39 -0600

    Formulario Registro

---
+++ Controllers/RegistroController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace INNOVAMED.Controllers
+{
+    public class RegistroController : Controller
+    {
+        // GET: Registro
+        public ActionResult RegistrarPaciente()
+        {
+            return View();
+        }
+    }
+}
