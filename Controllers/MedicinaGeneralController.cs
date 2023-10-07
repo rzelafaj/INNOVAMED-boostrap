229a5674a520c0c88fa75083fe299a9ff3237331
SALAZAR VENTURA CRISTIAN FERNANDO <2505462022@mail.utec.edu.sv>
2023-10-07 17:03:41 -0600

    #visa y controlador -medicina -general

---
+++ Controllers/MedicinaGeneralController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace INNOVAMED.Controllers
+{
+    public class MedicinaGeneralController : Controller
+    {
+        // GET: MedicinaGeneral
+        public ActionResult MedicinaGeneral()
+        {
+            return View();
+        }
+    }
+}
