beb5974f704c9873c1b7b3c66d3abf84099f7f33
rzelafaj <1736462009@mail.utec.edu.sv>
2023-10-16 13:59:06 -0600

    inicio de programacion de proyecto

--- Controllers/RegistroController.cs
+++ Controllers/RegistroController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+//asemos referencia al modelo de la solucion
+using INNOVAMED.Models;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace INNOVAMED.Controllers
 {
     public class RegistroController : Controller
     {
+        
         // GET: Registro
         public ActionResult RegistrarPaciente()
         {
---
+++ Models/Pacientes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace INNOVAMED.Models
+{
+    public class Pacientes
+    {
+       public int IdPaciente { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string DUI { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Responsable { get; set; }
+        public  bool Estado { get; set; }
+    }
+}
