78f923256cdd3824e843059de2bd5a02c3b6d142
TORRES RAMIREZ NELSON ARNOLDO <2513842022@mail.utec.edu.sv>
2023-10-17 12:48:45 -0600

    Especialidades

---
+++ Controllers/EspecialidadesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace INNOVAMED.Controllers
+{
+    public class EspecialidadesController : Controller
+    {
+        // GET: Especialidades
+        public ActionResult Cardiologia()
+        {
+            return View();
+        }
+
+
+        // GET: Especialidades
+        public ActionResult Pediatria()
+        {
+            return View();
+        }
+    }
+}
