2672bc614fab83a198186e6352d30886a0ffae1c
Stanley Ramirez <stanleyhernandez74@gmail.com>
2023-10-19 23:09:10 -0600

    Funcionalidad del Login

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+//asemos referencia al modelo de la solucion
+using INNOVAMED.Models;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+
 
 namespace INNOVAMED.Controllers
 {
@@ -13,5 +20,58 @@ namespace INNOVAMED.Controllers
         {
             return View();
         }
+
+        //utilizacion de metodo http post para llamado de medo y interacion con la base de datos 
+        [HttpPost]
+        public ActionResult AccederSistema(Pacientes P)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                SqlDataReader Lector = null;
+
+                try
+                {
+                    // instancia a la cadena de conexion
+                    SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["conexion"].ConnectionString);
+
+                    //objeto de tipo command type para interactuar con sql
+                    SqlCommand cmd = new SqlCommand("Select Email, Password from Pacientes " + "where Email = @Email and Password = @Password", con);
+                    cmd.CommandType = CommandType.Text;
+                    
+                    cmd.Parameters.Add("@Email", SqlDbType.VarChar).Value = P.Email;
+                    cmd.Parameters.Add("@Password", SqlDbType.VarChar).Value = P.Password;
+                    //abrimos la conexion
+                    con.Open();
+                    Lector = cmd.ExecuteReader();
+
+                    if (Lector.Read())
+                    {
+
+                        // Pasamos los datos obtenidos en el query
+                        P.Email = Lector["Email"].ToString();
+                        P.Password = Lector["Password"].ToString();
+
+                        con.Close();
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        con.Close();
+                        Response.Write("<script>alert('USUARIO O CONTRASEÑA INCORRECTO')</script>");
+                        return View("Login");
+                    }   
+
+                }
+                catch (Exception ex)
+                {
+                    return View("Login");
+                    throw ex;
+                }
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
     }
+    
 }
