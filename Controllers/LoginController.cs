5a0706bd291d0b772f61c3aba045416c85d90df4
SALAZAR SANCHEZ JONATHAN JOSUE <2517202022@mail.utec.edu.sv>
2023-10-28 22:33:16 -0600

    cambios confimados

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -59,7 +59,6 @@ namespace INNOVAMED.Controllers
                         P.Password = Lector["Password"].ToString();
                         //guardamos la sesion en un variable de session
                         Session["UsuarioLogueado"] = true;
-                        //guardamos el Id del Paciente en una varible de Session
                         Session["IdPaciente"] = Lector["IdPaciente"];
                         con.Close();
                         
@@ -78,7 +77,7 @@ namespace INNOVAMED.Controllers
                     throw ex;
                 }
             }
-            Response.Write("<script>alert('Bienvenido sesion iniciada')</script>");
+            Response.Write("<script>alert('Bienvenido secion iniciada')</script>");
             return RedirectToAction("Index", "Home");
         }
     }
